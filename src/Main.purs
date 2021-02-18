89e97f7b4ca57a91ec8e6c2e448adda9a093aac6
nathan <nathan.wilson@marquetteenergyanalytics.com>
2021-02-18 14:10:38 -0500

    what we had at the end of part 2

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,11 @@ module Main where
 
 import Prelude
 
+import Control.Monad.Except (ExceptT(..), except, lift, runExceptT)
+import Control.Monad.State (StateT)
 import Data.Either (Either(..))
 import Data.Interpolate (i)
-import Data.Newtype (class Newtype)
+import Data.Newtype (class Newtype, unwrap)
 import Effect (Effect)
 import Effect.Class.Console (log, logShow)
 
@@ -13,37 +15,58 @@ type Response = Int
 
 type ErrorMessage = String
 
-newtype EffectEither a b = EffectEither (Effect (Either a b))
-derive instance newtypeEffectEither :: Newtype (EffectEither a b) _
-instance monadEffectEither :: Monad (EffectEither a) 
+-- newtype EitherT a m b = EitherT (m (Either a b))
+-- derive instance newtypeEitherT :: Newtype (EitherT a m b) _
+-- instance monadEitherT :: Monad m => Monad (EitherT a m) 
 
-instance applicativeEffectEither :: Applicative (EffectEither err) where 
-  -- let pure (x:'a) : IOResult<'a, 'err>) = 
-  --   IOResult <| io { return result { return x } } 
-  pure :: forall a. a -> EffectEither err a
-  pure x = EffectEither $ pure $ pure x 
+-- instance applicativeEitherT :: Monad m => Applicative (EitherT err m) where 
+--   -- let pure (x:'a) : IOResult<'a, 'err>) = 
+--   --   IOResult <| io { return result { return x } } 
+--   pure :: forall a. a -> EitherT err m a
+--   pure x = EitherT $ pure $ pure x 
 
-instance bindEffectEither :: Bind (EffectEither err) where
-  bind :: forall a b. EffectEither err a -> (a -> EffectEither err b) -> EffectEither err b
-  bind (EffectEither (x :: Effect (Either err _))) fn = EffectEither $ do
-    (x' :: Either err _) <- x
-    case x' of
-      Left e -> pure $ Left e
-      Right x'' -> 
-        let (EffectEither (y :: Effect (Either err _))) = fn x''
-        in y
+-- instance bindEitherT :: Monad m => Bind (EitherT err m) where
+--   bind :: forall a b. EitherT err m a -> (a -> EitherT err m b) -> EitherT err m b
+--   bind (EitherT (x :: m (Either err _))) fn = EitherT $ do
+--     (x' :: Either err _) <- x
+--     case x' of
+--       Left e -> pure $ Left e
+--       Right x'' -> 
+--         let (EitherT (y :: m (Either err _))) = fn x''
+--         in y
 
-  -- let bind (EffectEither (x : IO<Result<'a, 'err>>)) (fn : 'a -> IOResult<'b, 'err>) : IOResult<'b, 'err> = IOResult <| io {
-  --   let! (x' : Result<'a, 'err>) = x
-  --   match x' with
-  --   | Error e -> return Error e
-  --   | Ok x'' ->
-  --     let (EffectEither (y : IO<Result<'b, 'err>>)) = fn x''
-  --     return! y
-  -- }
+--   -- let bind (EitherT (x : IO<Result<'a, 'err>>)) (fn : 'a -> IOResult<'b, 'err>) : IOResult<'b, 'err> = IOResult <| io {
+--   --   let! (x' : Result<'a, 'err>) = x
+--   --   match x' with
+--   --   | Error e -> return Error e
+--   --   | Ok x'' ->
+--   --     let (EitherT (y : IO<Result<'b, 'err>>)) = fn x''
+--   --     return! y
+--   -- }
 
-instance applyEffectEither :: Apply (EffectEither a) where apply = ap
-derive instance functorEffectEither :: Functor (EffectEither a) 
+-- instance applyEitherT :: Monad m => Apply (EitherT a m) where apply = ap
+-- derive instance functorEitherT :: Monad m => Functor (EitherT a m) 
+
+-- except :: forall err a m. Monad m => Either err a -> EitherT err m a
+-- except x = EitherT $ pure x
+-- -- let eitherToIOResult x = IOResult <| io { return x }
+
+-- lift :: forall err a m. Monad m => m a -> EitherT err m a
+-- lift x = EitherT $ do 
+--   x' <- x
+--   pure $ Right x'
+-- -- let lift (x:IO<'a>) : IOResult<'a, 'err> = IOResult <| io {
+-- --   let! (x':'a) = x
+-- --   return Ok x'
+-- -- }
+
+type EffectStateEither err state a = ExceptT err (StateT state Effect) a
+
+
+
+-- runEitherT :: forall err m a. Monad m => EitherT err m a -> m (Either err a)
+-- runEitherT = unwrap
+-- let runIOResult (IOResult x) = x
 
 ajaxRequest :: Effect (Either ErrorMessage Response)
 ajaxRequest = pure $ pure 5
@@ -58,14 +81,15 @@ writeResult :: Result -> Effect Unit
 writeResult r = logShow r
 
 run :: Effect (Either ErrorMessage Unit)
-run = do
-  resp <- ajaxRequest
-  case resp of 
-    Left err -> pure $ Left err
-    Right r ->
-      case (processResponse r) of 
-        Left err -> pure $ Left err
-        Right result -> writeResult result <#> Right
+run = runExceptT $ do
+  resp <- ExceptT ajaxRequest
+  result <- except (processResponse resp)
+  lift $ writeResult result 
+
+-- f# |>, <|, |>>, <<|, =<<, >>= (loadWx dateRange =<< getConnStr) 
+-- ps  #,  $, <#>, <$>, =<<, >>=
+-- hs  &,  $, <&>, <$>, =<<, >>=
+
 
 -- let run = io {
 --   let! resp = ajaxRequest
